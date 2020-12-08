9c0b08e1dac1677d4f1ab3169632913d5c3c1613
nagawa-hub <nakagawayu2008@gmail.com>
2020-12-08 22:07:07 +0900

    マイページの画像投稿機能を実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:edit,:update]
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to action: :show
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.required(:user).permit(:name,:user_image,:user_comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "books#index"
   
+  resources :users, only: [:show,:edit,:update]
   resources :books, only: [:index]
+
 end
