d08ac84f6a5b90cfe1ee367a8c44501b10b53e98
nagawa-hub <nakagawayu2008@gmail.com>
2020-12-30 17:39:23 +0900

    画像表示のため編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, 'honstagram'
 set :repo_url,  'git@github.com:nagawa-hub/honstagram.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/storage')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
