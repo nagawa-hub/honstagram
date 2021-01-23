b820ea19fe70d98321d84e75e1120c98f4d9c78c
nagawa-hub <nakagawayu2008@gmail.com>
2021-01-23 14:32:54 +0900

    JQueryコンソール表示OK

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,13 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 // ダッシュボード
 require("chartkick")
 require("chart.js") 
 
 // ブートストラップ
-//= require jquery3
 //= require popper
 //= require bootstrap
 
@@ -21,7 +21,7 @@ require("chart.js")
 require('./preview')
 require('./header_pulldown')
 require('./create_review')
-// require('./search_word')
+require('./search_word')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/packs/search_word.js
+++ app/javascript/packs/search_word.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const inputElement = document.getElementById("search-field");
-  inputElement.addEventListener("keyup", () => {
-    const keyword = document.getElementById("search-field").value;
-    const XHR = new XMLHttpRequest();
-    XHR.open("GET", `search/?keyword=${keyword}`, true);
-    XHR.responseType = "json";
-    XHR.send();
-  })
-  
-})
+$(function() {
+  console.log("jquery")
+});
