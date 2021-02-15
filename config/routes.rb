a807fc3ded275969f9c4225d127e310d8039c58a
nagawa-hub <nakagawayu2008@gmail.com>
2021-02-15 19:12:44 +0900

    revert "管理者ページを表示"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   devise_for :users
   post '/users/guest_sign_in', to: 'users#guest'
   root to: "books#top"
